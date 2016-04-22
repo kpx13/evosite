622f1c6dccdf12b817d303e576618ab05e913b96
annkpx <annkpx@gmail.com>
2016-04-23 01:37:19 +0300

    from server

--- evosite/settings.py
+++ evosite/settings.py
@@ -17,10 +17,10 @@ DATETIME_FORMAT = '%d.%m.%Y'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'evosite',   # Or path to database file if using sqlite3.
-        'USER': 'evosite',                      # Not used with sqlite3.
-        'PASSWORD': 'evosite',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'evomailing',   # Or path to database file if using sqlite3.
+        'USER': 'evomailing',                      # Not used with sqlite3.
+        'PASSWORD': '0zv0B1urNa',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
--- wsgi.py
+++ wsgi.py
@@ -2,13 +2,11 @@ import os, sys, site
 
 root = os.path.abspath(__file__)
 
-site_packages_root = os.path.join(root, '../lib/python2.6/site-packages')
+site_packages_root = os.path.join(root, '../lib/python2.7/site-packages')
 
 site.addsitedir(site_packages_root)
 sys.path = [os.path.dirname(root), root, site_packages_root] + sys.path
 os.environ['DJANGO_SETTINGS_MODULE'] = 'evosite.settings'
-
-print 'SYS PATH IS', sys.path
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
